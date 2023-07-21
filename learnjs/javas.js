7a1514cb84eccaf463d816bc54bca87702afde85
Sowmyasri16 <133667914+Sowmyasri16@users.noreply.github.com>
2023-07-21 14:11:55 +0530

    Add files via upload

---
+++ learnjs/javas.js
@@ -0,0 +1,9 @@
+let bread =prompt("your bread is:");
+let souce =prompt("your souce is");
+let veggie =prompt("you vegitable is");
+ function sandwizw(breads,souces,veggies)
+ {
+    let sanduy="your sandwidze is made up of" +breads +" and" +souces +" and "+veggies;
+    return sanduy;
+ }
+ function sandwizw(bread,souce,veggie);
